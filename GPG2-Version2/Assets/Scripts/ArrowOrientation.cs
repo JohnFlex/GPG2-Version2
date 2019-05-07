d37b659cac7cc75ecc558c7411153401f16886fe
John <marcelih55@gmail.com>
2019-05-07 22:37:14 +0200

    Update 2019 & Adding Gauge

--- GPG2-Version2/Assets/Scripts/ArrowOrientation.cs
+++ GPG2-Version2/Assets/Scripts/ArrowOrientation.cs
@@ -8,14 +8,12 @@ public class ArrowOrientation : MonoBehaviour
     public float rotationSpeed = 10f;
     public float forceDash = 10f;
     public GameObject ball;
+    public ScriptBarre scriptBarre;
     bool Tir = true;
     public int Nbdash = 3;
     public int NbMonoImpuler = 3;
 
-    void Start()
-    {
-        
-    }
+
     // Update is called once per frame
     void Update()
     {
@@ -25,19 +23,21 @@ public class ArrowOrientation : MonoBehaviour
 
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && (Nbdash > 0 || Tir))
+        NbMonoImpuler = scriptBarre.GetPower() + 2;
+
+        if (Input.GetButtonDown("Submit") && (Nbdash > 0 || Tir))
         {
             if (!Tir)
             {
                 Nbdash--;
             }
             Tir = false;
+
             for (int i = 0; i < NbMonoImpuler; i++)
             {
                 ball.GetComponent<Rigidbody>().AddForce((Orientateur.transform.position - ball.transform.position) * forceDash, ForceMode.Impulse);
 
             }
-            Debug.Log(Nbdash);
 
         }
 
@@ -46,4 +46,5 @@ public class ArrowOrientation : MonoBehaviour
 
 
     }
+
 }
---
+++ GPG2-Version2/Assets/Scripts/ScriptBarre.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptBarre : MonoBehaviour
+{
+
+    public Image Barre;
+    public RawImage Indicateur;
+    public RectTransform Center;
+    public float Speed;
+
+    private Vector2 centreBarre;
+    private Vector2 posBarre;
+
+    private bool vaVersLeBas = true;
+    private bool canShoot = true;
+    private int Power;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        centreBarre = new Vector2(Barre.rectTransform.rect.height / 2, Barre.rectTransform.rect.width / 2);
+        posBarre = new Vector2(Barre.rectTransform.position.x, Barre.rectTransform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vaVersLeBas && canShoot)
+        {
+            Indicateur.rectTransform.position += new Vector3(0, -Speed) * Time.deltaTime;
+        }
+        else if (canShoot)
+        {
+            Indicateur.rectTransform.position += new Vector3(0, Speed) * Time.deltaTime;
+        }
+
+        float posYIndic = Indicateur.rectTransform.localPosition.y;
+
+        if ((posYIndic >= Barre.rectTransform.rect.height/2)||(posYIndic <= -Barre.rectTransform.rect.height/2)){
+            vaVersLeBas = !vaVersLeBas;
+        }
+
+        if (Input.GetButtonDown("Submit"))
+        {
+            canShoot = false;
+            Debug.Log(GetPower());
+        }
+    }
+
+    public int GetPower()
+    {
+        return Power;
+    }
+
+    private void FixedUpdate()
+    {
+        Power = (Mathf.RoundToInt(100 - Vector2.Distance(Center.localPosition, Indicateur.rectTransform.localPosition))) /25 ;
+    }
+
+}
